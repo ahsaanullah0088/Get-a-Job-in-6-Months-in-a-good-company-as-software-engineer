712294097c5163b6256713541529a7872ba5d3a2
Ahsaan Ullah <ahsaanu70@gmail.com>
2024-08-23 04:03:39 -0700

    for loops in js

---
+++ Javascript/Javascript_baiscs/chapter3.js
@@ -0,0 +1,63 @@
+// loops 
+
+// loops are used to repeat the same code multiple times 
+
+// for loop
+// for loop is used to repeat the same code multiple times
+// for loop has 3 parts
+// 1. initialization
+// 2. condition
+// 3. increment/decrement
+// for (initialization; condition; increment/decrement) {
+//     // code to be executed
+// }
+ 
+
+// code 
+// for (let i = 0; i < 5; i++) {
+//     console.log(i);
+// }
+
+
+
+// calculate the sum of numbers from 1 to 10
+// let sum = 0;
+// for (let i = 1; i <= 10; i++) {
+//     sum += i;
+// }
+// console.log(sum);
+
+
+// infinite loop 
+ 
+
+// while loop 
+// while loop is used to repeat the same code multiple times
+// while loop has only one part
+// 1. condition
+// while (condition) {
+//     // code to be executed
+// }
+
+// let i = 0;
+// while (i < 5) {
+//     console.log(i);
+//     i++;
+// }
+
+
+// do while loop
+// do while loop is used to repeat the same code multiple times
+// do while loop has only one part
+// 1. condition
+// do {
+//     // code to be executed
+// } while (condition);
+
+
+let i = 0;
+do {
+    console.log(i);
+    i++;
+} while (i < 5);
+
