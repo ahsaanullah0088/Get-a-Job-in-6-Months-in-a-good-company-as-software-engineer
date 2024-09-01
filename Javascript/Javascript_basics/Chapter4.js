4c210d705c114abd95ea40b7fb905d6e42894aed
Ahsaan Ullah <ahsaanu70@gmail.com>
2024-08-31 23:55:42 -0700

    basic array

---
+++ Javascript/Javascript_basics/Chapter4.js
@@ -0,0 +1,50 @@
+// arrays
+
+
+//colletions of items 
+
+/// create array 
+
+// let heros= ['superman','batman','spiderman','ironman','captain america'];
+
+// let markst = [10,20,30,40,50,60,70,80,90,100];
+
+// let infor = ['john',25];
+
+
+// linear way to store the data are arrays 
+
+/// position , index matters in arrays 
+// 
+
+let marks = [10,20,30,40,50,60,70,80,90,100];
+ console.log(marks)
+
+
+ console.log(marks.length) // property of array to display the size of the array 
+
+
+
+ // type of marks 
+
+ console.log(typeof marks) //object 
+
+ // array indices 
+
+ /// array name [index],.....
+
+ /// 
+    console.log(marks[0]) //10
+    console.log(marks[1]) //20
+    console.log(marks[2]) //30
+    console.log(marks[3]) //40
+    console.log(marks[4]) //50
+
+    marks[0] = 100; // update the value at index 0
+
+    console.log(marks[0]) //100
+
+    console.log(marks[100]) // undefined 
+
+    // 
+
